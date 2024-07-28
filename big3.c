494a4ff25c6de615b87674197ad8a93dee0194af
Govu8890 <govardhanmt422000@gmail.com>
2024-07-28 12:44:01 +0530

    Update big3.c

--- big3.c
+++ big3.c
@@ -7,6 +7,7 @@ int main() {
     // if n1 is greater than both n2 and n3, n1 is the largest
     if (n1 >= n2 && n1 >= n3)
         printf("%.2f is the largest number.", n1);
+        print("dfvjdh)
 
     
     // if n2 is greater than both n1 and n3, n2 is the largest
